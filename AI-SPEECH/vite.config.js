07de2f36832a77d6c9f7507050f6d59552060177
kupendrav <kupendrav99@gmail.com>
2025-04-02 15:48:23 +0530

    set base URL for Vite configuration

--- AI-SPEECH/vite.config.js
+++ AI-SPEECH/vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: process.env.VITE_APP_BASE_URL || '/ROLEX_AI_PRO',
 })
